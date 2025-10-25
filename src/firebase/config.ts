2981863b02062da2af39c9dc0f520c59d13975f9
Ali KARACAN <ali.krcn@gmail.com>
2025-10-25 23:40:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-537578177-70262",
+  "appId": "1:480187512777:web:b25aa3297dbf61ba7a9a18",
+  "apiKey": "AIzaSyCK13tqvwlblQEv03WnIgPymx2_chMOpBw",
+  "authDomain": "studio-537578177-70262.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "480187512777"
+};
